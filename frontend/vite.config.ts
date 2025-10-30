bfe4e1945498a2f25f3b84ac657fda37c446c2dc
vujwalaujju <mvujwala@actalentservices.com>
2025-10-30 20:11:20 +0530

    changed the frontend port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   build: {
     outDir: "build",
   },
+  server: {
+    port: 5174,
+    strictPort: true,
+    host: true,
+  },
+  preview: {
+    port: 5174,
+    strictPort: true,
+  },
 });
