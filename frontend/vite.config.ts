0dfe784d551a5bce801e2baec8bdd0fb6c86cddf
vujwalaujju <mvujwala@actalentservices.com>
2025-10-31 13:44:45 +0530

    changes to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-  },
-  server: {
-    port: 5174,
-    strictPort: true,
-    host: true,
-  },
-  preview: {
-    port: 5174,
-    strictPort: true,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "build",
+      chunkSizeWarningLimit: 2000,
+    },
+    server: {
+      port: 5174,
+      strictPort: true,
+      host: true,
+    },
+    preview: {
+      port: 5174,
+      strictPort: true,
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(mode),
+    },
+  };
 });
