ca867745024535b38379f8852e2587657546e192
vujwalaujju <mvujwala@actalentservices.com>
2025-11-17 11:38:58 +0530

    fix changes in docker-compose

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ app.use(
     origin: [
       "http://localhost:5175",
       "http://localhost:3000",
+
+      "http://localhost:3001",
       "http://localhost:5055",
       "http://localhost:5174",
       "http://localhost:5173",
