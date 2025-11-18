cfe2606ad24f0768273693d0143f82e886e12735
vujwalaujju <mvujwala@actalentservices.com>
2025-11-18 12:13:45 +0530

    quick fix

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,19 @@ app.use(
       "http://localhost:3001",
       "http://localhost:5174",
       "http://localhost:5173",
+      "http://10.188.10.151:5296",
+      "http://localhost:5296",
       "http://10.188.10.151:3001",
       "http://10.188.10.151:5174",
       "http://10.188.10.151:5173",
       "http://10.188.10.151:5172",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
+app.options("*", cors());
 app.use(express.json());
 app.use(express.static("public"));
 
